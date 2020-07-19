c4bb64d42300d4e0e2bfbdb26a052ea7253865e7
gavinsharkey <gavinsharkey2016@gmail.com>
2020-07-19 10:33:35 -0400

    Add loading posts action to postsReducer Add ADD_POST action to postsReducer Add LIKE_POST action to postsReducer Add UNLIKE_POST action to postsReducer

--- nibbl_app_frontend/src/reducers/postsReducer.js
+++ nibbl_app_frontend/src/reducers/postsReducer.js
@@ -1,12 +1,64 @@
 export default (state = {
   posts: [],
-  current_page: 0
+  current_page: 0,
+  loadingPosts: false
 }, action) => {
   switch (action.type) {
+    case 'LOADING_POSTS':
+      return {
+        posts: state.posts,
+        current_page: state.current_page,
+        loadingPosts: true
+      }
     case 'SET_POSTS':
       return  {
         posts: action.posts,
-        current_page: action.page
+        current_page: action.page,
+        loadingPosts: false
+      }
+    case 'ADD_POST':
+      return {
+        current_page: state.current_page,
+        loadingPosts: false,
+        posts: [ action.post, ...state.posts ]
+      }
+    case 'LIKE_POST':
+      return {
+        current_page: state.current_page,
+        posts: state.posts.map(postData => {
+          if (postData.post.id === action.id) {
+            return {
+              liked_by_current_user: true,
+              post: {
+                ...postData.post,
+                user: { ...postData.post.user },
+                likes: [...postData.post.likes, action.like]
+              }
+            }
+          } else {
+            return postData
+          }
+        }),
+        loadingPosts: false
+      }
+    case 'UNLIKE_POST':
+      return {
+        current_page: state.current_page,
+        posts: state.posts.map(postData => {
+          if (postData.post.id === action.like.post_id) {
+            return {
+              liked_by_current_user: false,
+              post: {
+                ...postData.post,
+                user: { ...postData.post.user },
+                likes: postData.post.likes.filter(like => like.id !== action.like.id)
+              }
+            }
+          } else {
+            return postData
+          }
+        }),
+        loadingPosts: false
       }
     default:
       return state
