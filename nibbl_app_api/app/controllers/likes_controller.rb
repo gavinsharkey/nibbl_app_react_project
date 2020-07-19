079e5dd0199720993d8f84b7913dc2bf3cc24ed6
gavinsharkey <gavinsharkey2016@gmail.com>
2020-07-19 10:26:10 -0400

    Add likes model, controller

---
+++ nibbl_app_api/app/controllers/likes_controller.rb
@@ -0,0 +1,18 @@
+class LikesController < ApplicationController
+  def create
+    @like = Like.new(user: current_user, post_id: params[:like][:post_id])
+    if @like.save
+      render json: @like
+    else
+      render json: { errors: @like.errors.full_messages }
+    end
+  end
+
+  def destroy
+    @like = Like.find_by(id: params[:id])
+    @like.destroy
+    render json: @like
+  end
+
+  private
+end
