ffcb58d16c3bc58450460ee0aa30a6893740b770
gavinsharkey <gavinsharkey2016@gmail.com>
2020-07-19 10:40:04 -0400

    Change LOADING action to LOADING_USER

--- nibbl_app_frontend/src/actions/userActions.js
+++ nibbl_app_frontend/src/actions/userActions.js
@@ -2,7 +2,7 @@ import { fetchWithCredentials } from '../concerns/fetchable'
 
 const checkLoggedInStatus = () => {
   return dispatch => {
-    dispatch({type: 'LOADING'})
+    dispatch({type: 'LOADING_USER'})
     fetchWithCredentials('http://localhost:3001/api/v1/logged_in')
     .then(json => {
       if (json.logged_in) {
