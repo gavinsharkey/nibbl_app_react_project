a11da78c360ba05fc8aa151d08d17e90eff3d023
gavinsharkey <gavinsharkey2016@gmail.com>
2020-07-19 10:45:16 -0400

    Change posts.map to reflect new store arangement

--- nibbl_app_frontend/src/components/PostsScrollView.js
+++ nibbl_app_frontend/src/components/PostsScrollView.js
@@ -6,8 +6,8 @@ export default class PostsScrollView extends Component {
     const { posts, current_page } = this.props.postsData
     return (
       <div className="d-flex flex-column rounded posts">
-        { posts.map(post => (
-          <Post key={post.id} post={post} />
+        { posts.map(postData => (
+          <Post key={postData.post.id} post={postData.post} isLiked={postData.liked_by_current_user} />
         )) }
       </div>
     )
