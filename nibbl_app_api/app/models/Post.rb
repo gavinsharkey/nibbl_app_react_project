f9ebacc9acf1de6e4b95e849befc4b640f41d5cf
gavinsharkey <gavinsharkey2016@gmail.com>
2020-07-19 10:28:14 -0400

    Add validations to post model

--- nibbl_app_api/app/models/Post.rb
+++ nibbl_app_api/app/models/Post.rb
@@ -4,6 +4,8 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :users_that_liked, through: :likes, source: :user
 
+  validates :content, presence: true, length: { maximum: 30 }
+
   def self.from_current_users_followers(current_user, page=0)
     joins("INNER JOIN follows ON posts.user_id = follows.followed_user_id WHERE follows.follower_id = #{current_user.id}").order(created_at: :desc).limit(50).offset(page * 50)
   end
