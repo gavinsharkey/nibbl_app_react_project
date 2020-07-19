537cef9987d5b12e4cad76dc302ab484d414b2d1
gavinsharkey <gavinsharkey2016@gmail.com>
2020-07-19 10:27:57 -0400

    Add likes resource

--- nibbl_app_api/config/routes.rb
+++ nibbl_app_api/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   scope '/api/v1' do
     resources :users, only: [:create, :show, :update]
     resources :posts
+    resources :likes, only: [:create, :destroy]
     post '/login', to: 'sessions#create'
     delete '/logout', to: 'sessions#destroy'
     get '/logged_in', to: 'sessions#is_logged_in'
