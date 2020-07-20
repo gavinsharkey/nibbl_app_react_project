34c333af0d19909df122c48f5d1ff924b9c10a1b
gavinsharkey <gavinsharkey2016@gmail.com>
2020-07-20 13:20:06 -0400

    Add actions for commentsReducer

---
+++ nibbl_app_frontend/src/actions/commentsActions.js
@@ -0,0 +1,42 @@
+import { fetchWithCredentials } from '../concerns/fetchable'
+
+const fetchComments = postId => {
+  return dispatch => {
+    dispatch({type: 'LOADING_COMMENTS'})
+    fetchWithCredentials(`http://localhost:3001/api/v1/comments?post_id=${postId}`)
+    .then(json => {
+      dispatch({type: 'SET_COMMENTS', comments: json})
+    })
+  }
+}
+
+const createComment = (postId, body) => {
+  return dispatch => {
+    fetchWithCredentials('http://localhost:3001/api/v1/comments', 'POST', {
+      comment: {
+        post_id: postId,
+        body
+      }
+    })
+    .then(json => {
+      if (json.status === 200) {
+        dispatch({type: 'ADD_COMMENT', comment: json.comment})
+      }
+    })
+  }
+}
+
+const destroyComment = commentId => {
+  return dispatch => {
+    fetchWithCredentials(`http://localhost:3001/api/v1/comments/${commentId}`, 'DELETE')
+    .then(json => {
+      dispatch({type: 'DELETE_COMMENT', id: json.id})
+    })
+  }
+}
+
+export {
+  fetchComments,
+  createComment,
+  destroyComment
+}
