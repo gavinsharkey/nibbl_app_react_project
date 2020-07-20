89e2987060a850b284c40fa8346ef006e3c115d0
gavinsharkey <gavinsharkey2016@gmail.com>
2020-07-20 13:11:40 -0400

    Add posts by user_id option for posts index Change how liked_by_current_user is derived Add liked_by_current_user to posts show

--- nibbl_app_api/app/controllers/posts_controller.rb
+++ nibbl_app_api/app/controllers/posts_controller.rb
@@ -1,14 +1,25 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.from_current_users_followers(current_user, params[:page].to_i).to_a
-    @posts_with_current_user = @posts.concat(current_user.posts).sort_by { |post| post.created_at }.reverse
-    @posts_with_current_user_and_likes = @posts_with_current_user.map do |post|
-      {
-        post: post,
-        liked_by_current_user: !!Like.find_by(post: post, user: current_user) ? true : false
-      }
+    if params[:user_id]
+      @posts = Post.where(user_id: params[:user_id])
+      @posts_with_likes = @posts.map do |post|
+        {
+          post: post,
+          liked_by_current_user: !!post.likes.exists?(user: current_user)
+        }
+      end
+      render json: @posts_with_likes, include: [{user: { except: [:password_digest] }}, :likes], methods: :time_created_string
+    else
+      @posts = Post.from_current_users_followers(current_user, params[:page].to_i).to_a
+      @posts_with_current_user = @posts.concat(current_user.posts).sort_by { |post| post.created_at }.reverse
+      @posts_with_current_user_and_likes = @posts_with_current_user.map do |post|
+        {
+          post: post,
+          liked_by_current_user: !!post.likes.exists?(user: current_user)
+        }
+      end
+      render json: @posts_with_current_user_and_likes, include: [{user: { except: [:password_digest] }}, :likes], methods: :time_created_string
     end
-    render json: @posts_with_current_user_and_likes, include: [{user: { except: [:password_digest] }}, :likes], methods: :time_created_string
   end
 
   def create
@@ -29,7 +40,10 @@ class PostsController < ApplicationController
   def show
     @post = Post.find_by(id: params[:id])
     if @post
-      render json: @post, include: [{user: { except: [:password_digest] }}, :likes], methods: :time_created_string
+      render json: {
+        post: @post,
+        liked_by_current_user: !!@post.likes.exists?(user: current_user)
+      }, include: [{user: { except: [:password_digest] }}, :likes], methods: :time_created_string
     else
       render json: {
         status: 404,
