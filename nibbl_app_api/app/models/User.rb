6084574a4af4f926aac20732b8f0072839d13cc5
gavinsharkey <gavinsharkey2016@gmail.com>
2020-07-20 13:13:17 -0400

    User has_many comments Add followers_count method Add followings_count method

--- nibbl_app_api/app/models/User.rb
+++ nibbl_app_api/app/models/User.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
   has_many :likes
   has_many :liked_posts, through: :likes, source: :post
 
+  has_many :comments
+
   has_many :received_follows, foreign_key: :followed_user_id, class_name: 'Follow', dependent: :destroy
   has_many :followers, through: :received_follows, source: :follower
 
@@ -15,4 +17,11 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'must be valid'}
   
+  def followers_count
+    self.received_follows.count
+  end
+
+  def followings_count
+    self.given_follows.count
+  end
 end
