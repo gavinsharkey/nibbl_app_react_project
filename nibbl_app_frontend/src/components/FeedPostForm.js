7795a9dbccff781edcb22764a101662220664db9
gavinsharkey <gavinsharkey2016@gmail.com>
2020-07-22 16:18:38 -0400

    Add validations to post form change form style

--- nibbl_app_frontend/src/components/FeedPostForm.js
+++ nibbl_app_frontend/src/components/FeedPostForm.js
@@ -21,11 +21,12 @@ export default class FeedPostForm extends Component {
 
   render() {
     return (
-      <div className="post-form rounded p-3 my-2 border">
+      <div className="post-form p-3 my-2 border">
         <form onSubmit={this.handleSubmit} >
           <div className="form-group">
             <label>Content: </label>
-            <textarea onChange={this.handleChange} className="form-control p-3" placeholder="Make a post!" />
+            <textarea required maxLength={30} value={this.state.content} onChange={this.handleChange} className="form-control p-3" placeholder="Make a post!" />
+            <small>Characters Left: {30 - this.state.content.length}</small>
           </div>
           <div className="form-group">
             <input type="submit" value="Post" className="btn btn-primary" />
