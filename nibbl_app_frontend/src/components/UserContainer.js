d70487bb9c316401e7b61b6876e14cbdaf1ae074
gavinsharkey <gavinsharkey2016@gmail.com>
2020-07-22 16:25:18 -0400

    Implement follow functionality Add redirect to home page if user not found Add fetch user on update if params id changed

--- nibbl_app_frontend/src/components/UserContainer.js
+++ nibbl_app_frontend/src/components/UserContainer.js
@@ -1,29 +1,37 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { fetchPostsByUser, likePost, unlikePost } from '../actions/postsActions'
+import { fetchUser, followUser, unfollowUser } from '../actions/userActions'
 import PostsScrollView from './PostsScrollView'
 import Loading from './Loading'
+import UserHeader from './UserHeader'
+import { Redirect } from 'react-router-dom'
 
 class UserContainer extends Component {
   componentDidMount() {
     this.props.fetchPostsByUser(this.props.match.params.id)
+    this.props.fetchUser(this.props.match.params.id)
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.match.params.id !== prevProps.match.params.id) {
+      this.props.fetchPostsByUser(this.props.match.params.id)
+      this.props.fetchUser(this.props.match.params.id)
+    }
   }
 
   render() {
-    const { postsData, likePost, unlikePost } = this.props
+    const { match: { params: {id} }, followUser, unfollowUser, currentUserFollows, postsData, likePost, unlikePost, userData: { loadingUser, loadingFollow, userNotFound, user } } = this.props
+    if (userNotFound) {
+      return <Redirect to="/" />
+    }
+    const isFollowed = !!currentUserFollows.find(follow => follow.followed_user_id == id)
+
     return (
       <div className="mx-5 my-4 container">
-        <div className="user-header clearfix">
-          <div className="user-bio float-left">
-            <h1>Gavin Sharkey <small>@gavin</small></h1>
-            <p>Software Engineer | Dev | Florida</p>
-            <span className='d-inline-block mr-2'>Followers: 10</span>
-            <span className='d-inline-block mr-2'>Following: 16</span>
-          </div>
-          <div className="float-left my-2 mx-3">
-            <button className="btn btn-secondary">Follow</button>
-          </div>
-        </div>
+        { loadingUser
+        ? <Loading/>
+        : <UserHeader followUser={followUser} unfollowUser={unfollowUser} currentUserFollows={currentUserFollows} loadingFollow={loadingFollow} isFollowed={isFollowed} user={user} />}
         <div className="user-posts my-2 row">
           <div className="col-8">
             <h3>Posts: </h3>
@@ -39,8 +47,10 @@ class UserContainer extends Component {
 
 const mapStateToProps = state => {
   return {
-    postsData: state.postsData
+    postsData: state.postsData,
+    userData: state.userData,
+    currentUserFollows: state.currentUser.given_follows
   }
 }
 
-export default connect(mapStateToProps, { fetchPostsByUser, likePost, unlikePost })(UserContainer)
+export default connect(mapStateToProps, { fetchPostsByUser, fetchUser, followUser, unfollowUser, likePost, unlikePost })(UserContainer)
