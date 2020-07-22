d4fe072f252fadd97fd4f084fdc0ef34c25a6771
gavinsharkey <gavinsharkey2016@gmail.com>
2020-07-22 16:21:32 -0400

    Add NavLink to feed Add search bar

--- nibbl_app_frontend/src/components/NavBar.js
+++ nibbl_app_frontend/src/components/NavBar.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom'
+import SearchContainer from './SearchContainer'
 
 export default class NavBar extends Component {
   render() {
     return (
-      <header className="p-2 navbar">
-        <div className="buttons clearfix mx-2">
-          <button onClick={() => this.props.handleLogout()} className='float-right btn btn-outline-dark'>Logout</button>
+      <header className="p-2 mynavbar">
+        <div className="clearfix">
+          <button onClick={() => this.props.handleLogout()} className='btn btn-outline-dark'>Logout</button>
+          <NavLink to="/" className="float-right btn btn-outline-dark">Feed</NavLink>
+          <SearchContainer />
         </div>
       </header>
     )
