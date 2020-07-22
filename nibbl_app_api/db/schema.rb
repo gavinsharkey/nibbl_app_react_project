4e8219086134d7a7938d04386fc3d4869e599f89
gavinsharkey <gavinsharkey2016@gmail.com>
2020-07-22 16:09:24 -0400

    Add followers_count column to users

--- nibbl_app_api/db/schema.rb
+++ nibbl_app_api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_18_171750) do
+ActiveRecord::Schema.define(version: 2020_07_21_210709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2020_07_18_171750) do
     t.text "bio"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "followers_count"
   end
 
 end
