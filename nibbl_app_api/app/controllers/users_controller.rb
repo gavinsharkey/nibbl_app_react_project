7edf0f028c9dcee30d20ec57695882150677b40a
gavinsharkey <gavinsharkey2016@gmail.com>
2020-07-25 08:57:26 -0400

    Add set_user before action to users controller Add update, exists? actions to users controller

--- nibbl_app_api/app/controllers/users_controller.rb
+++ nibbl_app_api/app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update]
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -18,14 +20,24 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by(id: params[:id])
     if @user
-      render json: @user, except: [:password_digest], methods: [:followings_count]
+      render json: @user, except: [:password_digest], methods: [:followings_count, :followers_count]
     else
       render json: {
         status: 404,
         error: 'User Not Found'
       }
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      render json: @user, except: [:password_digest], methods: [:followings_count, :followers_count]
+    else
+      render json: { 
+        status: 500,
+        errors: @user.errors.full_messages
+       }
     end
   end
 
@@ -35,11 +47,22 @@ class UsersController < ApplicationController
   end
 
   def to_follow
-    @users = User.order('followers_count DESC').limit(3)
-    render json: @users, except: [:password_digest]
+    @users = User.by_most_followers
+    render json: @users, except: [:password_digest], methods: [:followers_count]
+  end
+
+  def exists?
+    @username_taken = User.exists?(username: params[:username])
+    render json: {
+      username_taken: @username_taken
+    }
   end
 
   private
+
+  def set_user
+    @user = User.find_by(id: params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:username, :email, :password, :password_confirmation, :display_name, :bio)
