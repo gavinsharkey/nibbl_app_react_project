0362b33822f582b0108cb3e2663cfbb8423826e4
gavinsharkey <gavinsharkey2016@gmail.com>
2020-07-25 09:14:04 -0400

    Change zIndex to fix positioning

--- nibbl_app_frontend/src/components/SearchItems.js
+++ nibbl_app_frontend/src/components/SearchItems.js
@@ -2,9 +2,9 @@ import React from 'react'
 import SearchItem from './SearchItem'
 
 export default function SearchItems(props) {
-  const { users, isSearching } = props.searchData
+  const { users } = props.searchData
   return (
-    <div className="position-absolute">
+    <div style={{zIndex: 1}} className="position-absolute">
       <ul className='list-group'>
         {users.map(user => (
           <SearchItem clearSearchBar={props.clearSearchBar} user={user} />
